8e4af2d1f39a7ee0e08ea2a1326394d17c5898f6
Eran <eran@cream-software.com>
2018-01-20 16:17:23 +0200

    removed current scripts.

--- testcraft-server/src/scripts/BAD.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-	print("Hello, World!", file=sys.stdout)
-	sys.stdout.flush()
-	print("FATAL ERROR", file=sys.stderr)
-	sys.stderr.flush()
-	sys.exit(1)
--- testcraft-server/src/scripts/good.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-	print("Hello, World!", file=sys.stdout)
-	sys.stdout.flush()
-	sys.exit(0)
-
